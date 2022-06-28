8fc264c78909568882ffa442c7ef3bf22adc30f3
mengruo <xumengruo@qq.com>
2022-06-28 11:32:29 +0800

    feat: 享元模式2

--- src/main/java/designpattern/flyweight/Forest.java
+++ src/main/java/designpattern/flyweight/Forest.java
@@ -8,7 +8,9 @@ package designpattern.flyweight;
 public class Forest {
 
     public void paintTree(String texture, String color, int x, int y) {
+        // 得到享元对象
         TreeType treeType = TreeFactory.getTreeType(texture, color);
+        // 利用享元对象和独有属性构造对象
         Tree tree = new Tree(treeType, x, y);
         tree.draw();
     }
